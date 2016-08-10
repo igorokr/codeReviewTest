547e6f391c06fe2e595f234fb90f89d1ee9587f9
iroman <iroman@softserveinc.com>
2016-08-10 16:43:51 +0300

    add file

---
+++ CodeReviewTest.java
@@ -0,0 +1,12 @@
+package com.idera.sqlinventorymanager.ui.dashboard;
+
+/**
+ * Created by iroman on 10.08.2016.
+ */
+public class CodeReviewTest {
+
+    public static void main(String[] args) {
+        System.out.println(" Line 1 ");
+    }
+
+}
